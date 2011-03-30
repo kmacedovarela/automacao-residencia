060f8bc09b7d72e3775789aafee790be65f0d726
Karina Macedo Varela <kmacedovarela@gmail.com>
2011-03-29 22:45:33 -0300

    iniciando alteracoes de layout e icones. mto a melhorar ainda!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,44 @@
 module ApplicationHelper
+  def link_voltar path
+    img = image_tag(
+        "/images/seta_esquerda.png",
+        :title => "Voltar",
+        :class => "voltar"
+     )
+     
+   link_to img, path
+  end
+  
+  def link_editar path
+     img = image_tag(
+         "/images/caneta.png",
+         :size => '16x16',
+         :title => "Editar",
+         :class => "icone"
+      )
+
+    link_to img, path
+  end
+  
+  def link_excluir path
+     img = image_tag(
+         "/images/menos.png",
+         :size => '16x16',
+         :title => "icone",
+         :class => "icone"
+      )
+
+    link_to img, path, :confirm => 'Você tem certeza?', :method => :delete 
+  end
+  
+  def link_visualizar path
+     img = image_tag(
+         "/images/lupa.png",
+         :size => '16x16',
+         :title => "Adicionar",
+         :class => "icone"
+      )
+
+    link_to 'LUPA', path
+  end
 end
