1761a69b0e5dba20f76e991297dfa46f6f96e9c3
Rodrigo Ramalho da Silva <hodrigohamalho@gmail.com>
2011-04-09 17:21:44 -0300

    insercao do ajax no form de usuarios

--- public/javascripts/views/usuario.js
+++ public/javascripts/views/usuario.js
@@ -1,3 +1,17 @@
+var usuario = {
+	renderizar_form: $("#novo_usuario").click(function(e){
+		e.preventDefault();
+		app.requisicao_ajax({url: $(this).attr('href')});
+		
+		usuario.mascaras();
+	}),
+	
+	mascaras: function(){
+    $('.cpf').mask('999.999.999-99');
+    $('.telefone').mask('(99) 9999-9999');
+  },
+}
+
 jQuery(function(){
 
 });
