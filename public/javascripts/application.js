c3386ecf06ac456b0e33debad9b7989e245318ee
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2011-04-17 21:04:02 -0300

    corrigindo erro de mascara, que deu com os .live

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,29 @@
+// esses var_applied tiveram de ser criados pois, por algum motivo, cada motivo
+// recebia diversas vzs a mesma mascará e acaba ocorrendo uns comportamentos
+// malucos.
+// TODO: Remover essa lógica do var_applied.
+
+var telefone_applied = false;
 $(".telefone").live('focus', function(){
-	$(this).mask('(99) 9999-9999');
+	if (!telefone_applied){
+	    $(this).mask('(99) 9999-9999');
+	    telefone_applied = true;
+	}
 });
 
+var cpf_applied = false;
 $(".cpf").live('focus', function(){
-	$(this).mask('999.999.999-99');
+	if (!cpf_applied){
+	    $(this).mask('999.999.999-99');
+	    cpf_applied = true;
+	}
 });
 
+var cep_applied = false;
 $(".cep").live('focus', function(){
-	$(this).mask('99999-999');
+	if (!cep_applied){
+	    $(this).mask('99999-999');
+	    cep_applied = true;
+	}
 });
+
