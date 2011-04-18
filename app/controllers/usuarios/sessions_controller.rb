bcf4ece89acb2e254893e29af5f698e2ba821200
Rodrigo Ramalho da Silva <hodrigohamalho@gmail.com>
2011-04-18 20:01:09 -0300

    autenticacao com devise

---
+++ app/controllers/usuarios/sessions_controller.rb
@@ -0,0 +1,31 @@
+class Usuarios::SessionsController < Devise::SessionsController
+
+  prepend_before_filter :require_no_authentication, :only => [ :new, :create ]
+  include Devise::Controllers::InternalHelpers
+
+  # GET /login
+  # def new
+  #   puts 'oi'
+  #   super
+  # end
+
+  # POST /login
+  def create
+    clean_up_passwords(build_resource)
+    if usuario_signed_in?
+      if current_usuario.role_administrador?
+        redirect_to :usuarios
+      elsif current_usuario.role_proprietario?
+        redirect_to :residencias_proprietario
+      end
+    else
+      super  
+    end
+  end
+
+  # # GET /logout
+  # def destroy
+  #   puts 'destroy'
+  #   super
+  # end
+end
