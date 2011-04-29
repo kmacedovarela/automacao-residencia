18b01c320c99e1281da6797af428aeba4bc13621
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2011-04-28 22:05:10 -0300

    corrigindo falha de seguranca nas controllers e seus respectivos testes

--- spec/controllers/cadastros_controller_spec.rb
+++ spec/controllers/cadastros_controller_spec.rb
@@ -1,13 +1,21 @@
 require 'spec_helper'
+include Devise::TestHelpers
 
 describe CadastrosController do
+
+  before do
+    usuario = Factory.create :usuario
+
+    sign_in usuario
+  end
+
   it 'deveria carregar o index' do
     get :index
     response.should render_template :index
-    
+
     assigns(:usuario).should_not be_nil
   end
-  
+
   it 'deveria criar um usuario' do
     hash = {
       :usuario => {
@@ -19,21 +27,21 @@ describe CadastrosController do
         :telefone => '(61) 8434-0652'
       }
     }
-    
+
     xhr :post, :create_usuario, hash
-  
+
     response.should be_success
     usuario = assigns(:usuario)
     usuario.should_not be_nil
     usuario.new_record?.should be_false
-    
+
     assigns(:residencia).should_not be_nil
-    assigns(:residencia).usuario.id.should eql usuario.id 
+    assigns(:residencia).usuario.id.should eql usuario.id
   end
-  
+
   it 'deveria cadastrar uma residencia' do
     usuario = Factory.create :usuario
-    
+
     hash = {
       :residencia => {
         :cidade => 'Brasilia',
@@ -43,38 +51,38 @@ describe CadastrosController do
         :usuario_id => usuario.id
       }
     }
-    
+
     xhr :post, :create_residencia, hash
-    
+
     res = assigns(:residencia)
     res.should_not be_nil
     res.should_not be_new_record
-    
+
     assigns(:comodo).should_not be_nil
   end
-  
+
   it 'deveria cadastrar um comodo' do
     residencia = Factory.create :residencia
-    
+
     hash = {
       :comodo => {
         :nome => 'Sala',
         :residencia_id => residencia.id
       }
     }
-    
+
     xhr :post, :create_comodo, hash
-    
+
     comodo = assigns(:comodo)
     comodo.should_not be_nil
     comodo.should_not be_new_record
-    
+
     assigns(:periferico).should_not be_nil
   end
-  
+
   it 'deveria cadastrar um periferico' do
     comodo = Factory.create :comodo
-    
+
     hash = {
       :periferico => {
         :nome => 'Lampada',
@@ -82,13 +90,14 @@ describe CadastrosController do
         :comodo_id => comodo.id
       }
     }
-    
+
     xhr :post, :create_periferico, hash
-    
+
     periferico = assigns(:periferico)
     periferico.should_not be_nil
     periferico.should_not be_new_record
-    
+
     assigns(:periferico).should_not be_nil
   end
 end
+
