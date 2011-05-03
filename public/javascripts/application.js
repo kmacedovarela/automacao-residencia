782a5f9a933383bd7036c2ee4ea9a320dc90d72a
Karina Macedo Varela <kmacedovarela@gmail.com>
2011-05-03 02:03:21 -0300

    mais refactorings de layout

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,11 +2,23 @@ var app = {
 
     selecionar_menu: function(id){
         $("div.nav ul#"+id).attr("class","select current");
-    }
+    },
+		
+		setup_botao_busca: function(){
+			var campo_busca = $("#campo_busca_home");
+			campo_busca.blur(function(){
+				if (this.value=='') { this.value='Buscar'; }
+			}); 
+			
+			campo_busca.focus(function(){
+				if (this.value=='Buscar') { this.value=''; }
+			});
+		}
 
 }
 
 $(document).ready(function(){
     $(document).pngFix( );
+		app.setup_botao_busca();
 });
 
