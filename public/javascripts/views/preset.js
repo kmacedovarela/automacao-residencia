37e72caa862b716cf12af48a515470d83afb74c3
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2011-05-26 11:29:44 -0300

    alteracao na migration de presets; mudanca nas rotas e inicio da insercao de presets

---
+++ public/javascripts/views/preset.js
@@ -0,0 +1,34 @@
+jQuery(function(){
+    $(".perifericos").click(function(){
+        if ($(this).attr("class") == "perifericos ligado"){
+            $(this).attr("class","perifericos");
+            $(this).find(".estado").attr('value', 'false');
+        }else{
+            $(this).attr("class","perifericos ligado");
+            $(this).find(".estado").attr('value', 'true');
+        }
+
+    });
+
+
+    $("#submit_preset").click(function(){
+        alert($("#form_cadastro_preset").serialize());
+
+        jQuery.ajax({
+			url: '/preset',
+			type: 'POST',
+			data: 'id='+periferico_id,
+			beforeSend: function() {
+				$('#periferico_'+periferico_id).html("<img src='/images/ajax-loader.gif' alt='espere por favor...' style='align: center;'></img>");
+			},
+
+			error: function(jqXHR, textStatus, errorThrown) {
+			    alert('erro');
+                alert(jqXHR.responseText);
+			}
+		});
+
+    });
+
+});
+
