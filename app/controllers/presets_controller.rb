614a0fcddbfa9ec360b58cfe1a8c2668bc043259
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2011-05-27 02:45:53 -0300

    create de preset

--- app/controllers/presets_controller.rb
+++ app/controllers/presets_controller.rb
@@ -10,7 +10,7 @@ class PresetsController < ApplicationController
 
     @comodos.each do |c|
       c.perifericos.each do |p|
-        p.estado = false;
+        p.estado = false
       end
     end
 
@@ -18,13 +18,39 @@ class PresetsController < ApplicationController
   end
 
   def create
+
     ids = params[:id]
     estados = params[:estado]
 
-    Preset.new :residencia =>
+    preset = Preset.new :nome => params[:nome]
 
-    redirect_to "/proprietarios/#{params[:residencia_id]}"
+    preset.estado_perifericos = to_hash ids, estados
+
+    if preset.estado_perifericos.blank?
+      flash[:error] = "Parametros inválidos."
+      redirect_to new_usuario_residencia_preset_url(current_usuario,current_usuario.residencia)
+    else
+      preset.save
+      flash[:notice] = "Preset cadastrado com sucesso."
+
+      redirect_to "/proprietarios/#{params[:residencia_id]}"
+    end
   end
+
+  private
+
+    def to_hash keys, values
+      hash = "{"
+      if keys.size == values.size
+        keys.each_with_index do |id, i|
+          hash += "':#{id}' => '#{values[i]}'"
+          hash += "," if not i == keys.size-1
+        end
+      end
+      hash += "}"
+
+      eval(hash)
+    end
 
 end
 
