e2536ff6445551d67643319065535dfade7699c3
Rodrigo Ramalho da Silva <hodrigohamalho@gmail.com>
2011-06-13 23:53:25 -0300

    corrigindo bug das mensagens do login nao estarem aparecendo

--- public/javascripts/views/login.js
+++ public/javascripts/views/login.js
@@ -1,4 +1,23 @@
+var login = {
+	comportamento_mensagens: function(id){
+		if ($(id).html().trim() != ""){
+			$(id).show();
+		}else{
+			$(id).hide();
+		}
+
+		$(id).click(function(){
+			$(id).slideUp();
+		});	
+	}
+
+}
+
 jQuery(function(){
 	$('#usuario_email').focus();
+
+	login.comportamento_mensagens("#loginbox_error");
+	login.comportamento_mensagens("#loginbox_alert");
+
 });
 
